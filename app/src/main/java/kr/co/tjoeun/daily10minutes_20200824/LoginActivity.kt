c8fa57c931b2f0f58feed75f3e8c890fbe9ecddd
이용찬 <lee9890@naver.com>
2020-08-25 21:04:28 +0900

    메인화면 => LoginActivity.로 변경

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/LoginActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/LoginActivity.kt
@@ -1,19 +1,17 @@
 package kr.co.tjoeun.daily10minutes_20200824
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
 import org.json.JSONObject
-import kotlin.math.log
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         setupEvents()
         setValues()
     }
@@ -30,21 +28,27 @@ class MainActivity : BaseActivity() {
 //                    실제 응답이 돌아왔을때 실행시켜줄 내용
                     
                     val codeNum = json.getInt("code")
-                    //val messageStr = json.getString("message")
                     
                     if (codeNum == 200){
 //                        서버 개발자가 => 로그인 성공일때는 code를 200으로 준다.
                         Log.d("로그인 시도", "성공 상황")
-//                        토스트도 일종의  UI 영향 코드  => runOnUiThread 실행 
+
+                        val data = json.getJSONObject("data")
+                        val user = data.getJSONObject("user")
+                        val nick_name = user.getString("nick_name")
+
+//                        토스트도 일종의  UI 영향 코드  => runOnUiThread 실행
                         runOnUiThread {
-                            Toast.makeText(mContext, "로그인성공", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, "닉네임 : ${nick_name}", Toast.LENGTH_SHORT).show()
                         }
                     } else {
 //                        로그인 실패
                         Log.e("로그인 시도","실패상황")
+
+                        val message = json.getString("message")
 //
                         runOnUiThread {
-                            Toast.makeText(mContext, "로그인실패", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
                         }
                     }
 
