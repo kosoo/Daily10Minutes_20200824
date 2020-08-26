3f060c94c0df037fc9acb8c57773049330f88ac9
이용찬 <lee9890@naver.com>
2020-08-26 21:53:21 +0900

    로그인 사용자 토큰 저장/조회 기능 추가

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ContextUtil.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/utils/ContextUtil.kt
@@ -10,16 +10,26 @@ class ContextUtil {
 
         private val AUTO_LOGIN_CHECK = "AUTO_LOGIN_CHECK"
 
-        fun setAutoLoginCheck(context: Context, isAuto : Boolean){
+        private val LOGIN_USER_TOKEN = "LOGIN_USER_TOKEN"
 
+        fun setAutoLoginCheck(context: Context, isAuto : Boolean){
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             pref.edit().putBoolean(AUTO_LOGIN_CHECK, isAuto).apply() //수정모드
-
         }
 
         fun getAutoLoginCheck(context: Context) : Boolean {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getBoolean(AUTO_LOGIN_CHECK, false)
+        }
+
+        fun setLoginUserToken(context: Context, token : String) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(LOGIN_USER_TOKEN, token).apply()
+        }
+
+        fun getLoginUserToken(context: Context) : String {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getString(LOGIN_USER_TOKEN, "")!!
         }
     }
 
