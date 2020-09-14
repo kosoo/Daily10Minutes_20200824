780e948347d365fa6a2d2e7254f583b3fbf9e38f
이용찬 <lee9890@naver.com>
2020-09-14 21:41:24 +0900

    프로젝트 제목/설명/이미지만 우선 반영

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
@@ -2,8 +2,14 @@ package kr.co.tjoeun.daily10minutes_20200824
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_project_detail.*
+import kr.co.tjoeun.daily10minutes_20200824.datas.Project
 
 class ViewProjectDetailActivity : BaseActivity() {
+
+    lateinit var mProject : Project
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_project_detail)
@@ -13,5 +19,16 @@ class ViewProjectDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProject = intent.getSerializableExtra("project") as Project
+
+        Glide.with(mContext).load(mProject.imageUrl).into(projectImg)
+
+        titleTxt.text = mProject.title
+        desctiponTxt.text = mProject.description
+
+
+
+
     }
 }
