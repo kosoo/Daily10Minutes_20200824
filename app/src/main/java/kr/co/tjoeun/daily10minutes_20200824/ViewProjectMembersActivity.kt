466f3aba962ba97e6b49788fed07da472ff5f260
이용찬 <lee9890@naver.com>
2020-09-15 21:17:24 +0900

    서버에서 멤버목록 파싱 => User클래스 별도 기능 추가 예정

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectMembersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectMembersActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_project_detail.*
 import kr.co.tjoeun.daily10minutes_20200824.datas.Project
 import kr.co.tjoeun.daily10minutes_20200824.datas.User
+import kr.co.tjoeun.daily10minutes_20200824.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewProjectMembersActivity : BaseActivity() {
 
@@ -35,6 +37,25 @@ class ViewProjectMembersActivity : BaseActivity() {
 //    서버에서 프로젝트의 참여 멤버 불러오는 기능
     fun getProjectMembersFromServer(){
 
+        ServerUtil.getRequestProjectMemberById(mContext, mProject.id, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val projectObj = data.getJSONObject("project")
+
+                val ongoingUserArr = projectObj.getJSONArray("ongoing_users")
+
+                for (i in 0 until ongoingUserArr.length()){
+
+                    val memberObj = ongoingUserArr.getJSONObject(i)
+
+//                    memberObj => User 형태로 변환 => ArrayList에 추가
+
+
+                }
+
+            }
+        })
 
     }
 }
