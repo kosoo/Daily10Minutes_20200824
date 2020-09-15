aaf64903885d81745eb15fa14a14565c937dad34
이용찬 <lee9890@naver.com>
2020-09-15 20:08:42 +0900

    프로젝트데이터 UI반영 기능 별도 함수로 분리 (모듈화)

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/ViewProjectDetailActivity.kt
@@ -32,9 +32,26 @@ class ViewProjectDetailActivity : BaseActivity() {
 //                실제로 서버에 신청처리
                 ServerUtil.postRequestApplyProject(mContext, mProject.id, object : ServerUtil.JsonResponseHandler{
                     override fun onResponse(json: JSONObject) {
-                        runOnUiThread {
-                            Toast.makeText(mContext, "프로젝트 참가 신청완료", Toast.LENGTH_SHORT).show()
+//                        자동 새로고침이 구현은 되지만 -> 서버를 한더 요청하기 때문에
+//                        신청 결과에서 알려주는 데이터를 화면에 반영
+//                        getProjectDetailFromServer()
+
+                        val code = json.getInt("code")
+
+                        if (code == 200) {
+                            val data = json.getJSONObject("data")
+                            val projectObj = data.getJSONObject("project")
+
+                            mProject = Project.getProjectFromJson(projectObj)
+
+                            runOnUiThread {
+                                Toast.makeText(mContext, "프로젝트 참가 신청완료", Toast.LENGTH_SHORT).show()
+
+//                                인원수 / 참가 버튼 등 UI 변경
+                                refreshProjectDataUI()
+                            }
                         }
+
                     }
 
                 })
@@ -70,32 +87,37 @@ class ViewProjectDetailActivity : BaseActivity() {
                 mProject = Project.getProjectFromJson(projectObj)
 
                 runOnUiThread {
-                    proofMethodTxt.text = mProject.proofMethod
-                    onGoingMemberCountTxt.text = "(현재 참여 인원 : ${mProject.onGoingMemberCount}명"
-
-//                    myLastStatus =>  마지막으로 변경된 프로젝트 신청상태
-//                    NULL : 신청한적이 없다.
-//                    ONGOING : 신청해서 진행중
-//                    FAIL : 중도포기, 3일 연속 인증글 X (자동 포기)
-//                    COMPLETE : 66일 짜리 모두 수행 완료 (프로젝트마다 다름)
-
-//                    신청하기 언제? 그 외의 모든 상황
-//                    중도 포기 언제? 상태가 ONGING일때
-
-                    if (mProject.myLastStatus == "ONGOING") {
-                        giveUpBtn.isEnabled = true
-                        applyBtn.isEnabled = false
-                    } else {
-                        giveUpBtn.isEnabled = false
-                        applyBtn.isEnabled = true
-
-                    }
-
-
+                    refreshProjectDataUI()
                 }
 
             }
         })
 
     }
+
+//    서버에서 준 프로젝트 정보 (내용이 변경된 mProject)를 => UI에 새로 반영하는 기능
+    fun refreshProjectDataUI(){
+
+        proofMethodTxt.text = mProject.proofMethod
+        onGoingMemberCountTxt.text = "(현재 참여 인원 : ${mProject.onGoingMemberCount}명"
+
+    //                    myLastStatus =>  마지막으로 변경된 프로젝트 신청상태
+    //                    NULL : 신청한적이 없다.
+    //                    ONGOING : 신청해서 진행중
+    //                    FAIL : 중도포기, 3일 연속 인증글 X (자동 포기)
+    //                    COMPLETE : 66일 짜리 모두 수행 완료 (프로젝트마다 다름)
+
+    //                    신청하기 언제? 그 외의 모든 상황
+    //                    중도 포기 언제? 상태가 ONGING일때
+
+        if (mProject.myLastStatus == "ONGOING") {
+            giveUpBtn.isEnabled = true
+            applyBtn.isEnabled = false
+        } else {
+            giveUpBtn.isEnabled = false
+            applyBtn.isEnabled = true
+
+        }
+    }
+
 }
